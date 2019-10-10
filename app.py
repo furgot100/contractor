3764f6e670808d71aed902f7c0168e11a8cd5d06
Francis Tsang <frtsang40@gmail.com>
2019-10-10 11:59:50 -0700

    docstrings

--- app.py
+++ app.py
@@ -11,15 +11,17 @@ client = MongoClient(host=f"{host}?retryWrites=false")
 db = client.get_default_database()
 
 players_collection = db.players
-
+# HOMEPAGE
 @app.route('/')
 def index():
     return render_template('index.html', players=players_collection.find())
 
+# CREATE
 @app.route('/new')
 def new_player():
     return render_template('new_player.html')
 
+# CREATE 
 @app.route('/new', methods=['POST'])
 def create_player():
     player = {
@@ -30,11 +32,13 @@ def create_player():
     player_id = players_collection.insert_one(player).inserted_id
     return redirect(url_for('show_player', player_id=player_id))
 
+# READ
 @app.route('/player/<player_id>')
 def show_player(player_id):
     player = players_collection.find_one({'_id': ObjectId(player_id)})
     return render_template('show_player.html', player=player)
 
+# UPDATE
 @app.route('/edit/<player_id>', methods=['POST'])
 def update_player(player_id):
     
@@ -49,11 +53,14 @@ def update_player(player_id):
     )
     return redirect(url_for('show_player', player_id=player_id))
 
+# UPDATE
 @app.route('/edit/<player_id>', methods=['GET'])
 def edit_player(player_id):
     player = players_collection.find_one({'_id': ObjectId(player_id)})
     return render_template('edit_player.html', player=player)
 
+
+# DELETE
 @app.route('/delete/<player_id>', methods=['POST'])
 def delete_player(player_id):
     players_collection.delete_one({'_id': ObjectId(player_id)})
